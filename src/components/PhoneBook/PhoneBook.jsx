340213a848c19fc9e8268b4313e04a622bbbd633
alex <lewyk@hotmail.com>
2021-02-28 21:12:04 +0200

    clear contacts

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -5,10 +5,7 @@ import ContactList from '../ContactList';
 import { Container, Typography } from '@material-ui/core';
 
 function PhoneBook() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  ]);
+  const [contacts, setContacts] = useState([]);
 
   const [find, setFind] = useState('');
 
