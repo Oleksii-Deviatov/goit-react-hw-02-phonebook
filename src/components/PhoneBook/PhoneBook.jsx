a5f0f2ce8d2a67aa020121d634228fa412401de0
alex <lewyk@hotmail.com>
2021-02-28 15:52:46 +0200

    clr contacts state

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -4,12 +4,7 @@ import Filter from '../Filter';
 import ContactList from '../ContactList';
 
 function PhoneBook() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState([]);
 
   const [find, setFind] = useState('');
 
